0f6df441b0243bcc2b11d654148e267163eb25ee
yulmaso <plotnik97@mail.ru>
2020-04-11 17:42:33 +0300

    Initial commit

---
+++ app/src/main/java/com/yulmaso/kskfact/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yulmaso.kskfact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
